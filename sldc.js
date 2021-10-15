1b6a99cab988c03732517c85934449206700ab9d
GulenYilmaz <gulenaltintasyilmaz@gmail.com>
2021-10-15 15:15:27 -0400

    sldc.js is done

---
+++ sldc.js
@@ -0,0 +1,26 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+
+// Planning
+// Analysis of Requirements
+// Design
+// Implementation
+// Testing & Integration
+// Maintain
+
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+*/
+
+let Planning = "In the Planning phase, project leaders evaluate the terms of the project. This includes calculating labor and material costs, creating a timetable with target goals, and creating the project’s teams and leadership structure."
+let AnalysisOfRequirements = "Defining requirements is considered part of planning to determine what the application is supposed to do and its requirements. "
+let Design ="The Design phase models the way a software application will work. Some aspects of the design include:Architecture,User Interface,Platforms,Programming,Communications,Security"
+let Implementation ="This is the actual writing of the program. A small project might be written by a single developer, while a large project might be broken up and worked by several teams."
+let TestingIntegration ="It’s critical to test an application before making it available to users. Much of the testing can be automated, like security testing."
+let Maintain ="In the deployment phase, the application is made available to users. Many companies prefer to automate the deployment phase. "
