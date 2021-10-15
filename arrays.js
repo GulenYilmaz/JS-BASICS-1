5a32fa4965ae3e3a397e226a422e516ee6d0dfd4
GulenYilmaz <gulenaltintasyilmaz@gmail.com>
2021-10-15 16:27:48 -0400

    arrays.js is done

--- arrays.js
+++ arrays.js
@@ -1,37 +1,50 @@
 //////////////////PROBLEM 1////////////////////
 /* Create a copy of the faveColors array called 'colorCopy' using the slice method. */
-
 const faveColors = ['red', 'green', 'black']
-
 //CODE HERE
+var colorCopy =faveColors.slice(0,2)
+
+
 
 //////////////////PROBLEM 2////////////////////
 /* Add a fourth color to the end of the 'colorCopy' array using the push method. */
-
 //CODE HERE
+var colorCopy = colorCopy.push('blue')
+
+
+
+
 
 //////////////////PROBLEM 3////////////////////
 /*
   Using an array method, create a new array called 'middleNums' from the 
   numbers array that will capture only the middle numbers (2, 3, 4).
 */
-
 const numbers = [1, 2, 3, 4, 5]
-
 //CODE HERE
+var middleNums =numbers.slice(1,3)
+
+
+
 
 
 //////////////////PROBLEM 4////////////////////
 /* 
-  Create a function called 'bigOrSmall' that takes in one parameter, 'arr', 
-  which will be an array of numbers. 
-  Inside of the bigOrSmall function, create a new array called 'answers'. 
-  Then, loop over the passed in arr parameter, and check to see if the number in the
-  array is GREATER than 100. 
+  Create a function called 'bigOrSmall' that takes in one parameter, 'arr', which will be an array of numbers. 
+  Inside of the bigOrSmall function, create a new array called'answers'. 
+  Then, loop over the passed in arr parameter, and check to see if the number in the array is GREATER than 100. 
   If it is, push 'big' as a string to the answers array. 
-  If the number is LESS than or EQUAL to 100, push 'small' as a string 
-  to the answers array. 
+  If the number is LESS than or EQUAL to 100, push 'small' as a string to the answers array. 
   Return the answers array inside of the function.
 */
 
 // CODE HERE
+function bigOrSmall(answers){
+
+  var small = true;
+  answers.forEach(item => {
+    (item <= 100) ? small = true : false;
+  });
+  (small == true) ? answers.push('big') : answers.push('small');
+  return answers
+}
